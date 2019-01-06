696ca80278edc1a90b571b131026d102e61b418e
gutsout <eratut@mail.ru>
2019-01-06 17:32:50 +0300

    Progress commit

--- index.php
+++ index.php
@@ -20,11 +20,74 @@ $response = $service->files->export($config['googleDriveFileId'], 'text/csv');
 $content = (string) $response->getBody();
 $words = explode("\r\n", $content);
 
-$strategy = new CambridgeDictionary('', new Crawler());
+$strategy = new CambridgeDictionary(new Crawler());
 $grabber = new DataGrabber($strategy);
 $data = [];
 foreach ($words as $word) {
     $strategy->setWordToFind($word);
     $data[] = $grabber->grabRemotely(new Client());
 }
-$f = 1;
+
+$toCSV = [
+    ['anchor_text', 'association_text']
+];
+$maxSentencesCols = 0;
+foreach ($data as $row) {
+
+    if (!isset($row['English'])) {
+        continue;
+    }
+
+    foreach ($row['English'] as $word) {
+        $anchorText = "{$word[CambridgeDictionary::DATA_KEY_WORD]} ({$word[CambridgeDictionary::DATA_KEY_TYPE_OF_WORD]})";
+
+        $associationText = '';
+        $examples = [];
+
+        foreach ($word[CambridgeDictionary::DATA_KEY_MEANINGS_AND_EXAMPLES] as $i =>$meaningsAndExamples) {
+            $number = count($word[CambridgeDictionary::DATA_KEY_MEANINGS_AND_EXAMPLES]) > 1 ? $i + 1 . '. ' : '';
+            $associationText .= "{$number}{$meaningsAndExamples[CambridgeDictionary::DATA_KEY_MEANING]}" . PHP_EOL;
+
+            $examples[] = $meaningsAndExamples[CambridgeDictionary::DATA_KEY_EXAMPLES];
+        }
+
+        /** @noinspection SlowArrayOperationsInLoopInspection */
+        $examples = array_merge(...$examples);
+        $examplesCount = count($examples);
+        $maxSentencesCols = $examplesCount > $maxSentencesCols ? $examplesCount : $maxSentencesCols;
+        $maxSentencesCols = $maxSentencesCols > 9 ? 9 : $maxSentencesCols;
+
+        $examples = array_slice($examples, 0, 9);
+
+        for ($i = 0; $i < 9; $i++) {
+            if (!isset($examples[$i])) {
+                $examples[$i] = ' ';
+            }
+        }
+
+        foreach ($examples as &$example) {
+            $example = str_replace($word[CambridgeDictionary::DATA_KEY_WORD], "*{$word[CambridgeDictionary::DATA_KEY_WORD]}*", $example);
+        }
+        unset($example);
+
+        $csvRow = [$anchorText, $associationText];
+        foreach ($examples as $example) {
+            $csvRow[] = $example;
+        }
+        $toCSV[] = $csvRow;
+    }
+}
+
+for ($i = 1; $i <= $maxSentencesCols; $i++) {
+    $toCSV[0][] = "sentence_{$i}_text";
+}
+
+$fp = fopen('result.csv', 'wb');
+
+foreach ($toCSV as $fields) {
+    fputcsv($fp, $fields);
+}
+
+fclose($fp);
+
+echo 'Done', PHP_EOL;
