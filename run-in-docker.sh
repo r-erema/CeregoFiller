d0fad60b66de5f402a536df70fbcc972bb05a85c
roman.erema <roman.erema@synesis.ru>
2020-02-14 15:59:14 +0300

    Update composer

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker run -v $(pwd):/var/www erema/php7.3-cli:latest php index.php
+docker run -v $(pwd):/var/www erema/php-cli:latest php index.php
 
 #docker run -v $(pwd):/var/www -u $(id -u):$(id -g) erema/php7.3-cli:latest php composer.phar require r.erema/data-grabber
